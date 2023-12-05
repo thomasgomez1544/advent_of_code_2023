c68882d44f6f022f68dc05b29b7fc5660664303d
TC Gomez <thomasgomez1544@gmail.com>
2023-12-04 22:21:20 -0600

    Created replaceNums function

--- day_3/index1.js
+++ day_3/index1.js
@@ -1,3 +1,38 @@
 const filePath = "day_3/data.txt";
 
 const testFilePath = "day_3/test_data.txt";
+
+const stringArray = generateArray(testFilePath);
+replaceNums(stringArray);
+
+function generateArray(path) {
+  const fs = require("fs");
+  const fileContents = fs.readFileSync(path, "utf8");
+  let stringArray = fileContents.trim().split(/\r?\n/);
+  return (stringArray = stringArray.map((element) => element.split("")));
+}
+
+function replaceNums(array) {
+  array.map((element) => {
+    let tepNumberStore = [];
+    for (let i = 0; i < element.length; i++) {
+      if (element[i] === ".") {
+        if (tepNumberStore.length === 0) {
+          continue;
+        }
+        let newNumber = tepNumberStore.join("");
+        for (let j = i - newNumber.length; j < i; j++) {
+          element[j] = Number(newNumber);
+        }
+        tepNumberStore = [];
+      } else if (isNaN(element[i])) {
+        continue;
+      } else {
+        tepNumberStore.push(element[i]);
+      }
+    }
+    return element;
+  });
+}
+
+function findPartNums(array) {}
